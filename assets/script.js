5aef68f1ef1d16da468f29205d4486a621689f59
Monica Howard <jay.m.howard@gmail.com>
2019-07-14 01:14:58 -0500

    created playGame() function w/ random word and dynamically created letter blanks

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+let movies = [
+  "Pulp Fiction",
+  "Shaft",
+  "Jungle Fever",
+  "Snakes on a Plane",
+  "Unbreakabe",
+  "The Incredibles",
+  "Django Unchained",
+  "Coach Carter",
+  "A Time to Kill"
+];
+
+
+let randomWord = "";
+let wordLength = [];
+let letterCard = 0;
+let wordInProgress = [];
+
+
+let playGame = () => {
+
+  let randomWord = movies[Math.floor(Math.random() * movies.length)];
+  let letters = randomWord.split("");
+  let letterBlanks = letters.length;
+
+  console.log(randomWord);
+  console.log(letters);
+  console.log("LetterBlanks", letterBlanks);
+
+  for (let i = 0; i < letterBlanks; i++) {
+    wordInProgress.push("_");
+  }
+  console.log("Word In Progress", wordInProgress);
+
+  document.querySelector("#letterCard").innerHTML = wordInProgress.join(" ");
+}
+
+playGame();
